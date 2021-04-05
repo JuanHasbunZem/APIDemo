a4f6dc39642ba6b0d8eab71da7f9d3792c5341c7
JuanHasbunZem <zemenarwow@gmail.com>
2021-04-04 23:17:11 -0700

    add divs for results, and two types of error messages and functions for when to display them

--- src/main.js
+++ src/main.js
@@ -2,8 +2,48 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Exchange from './currency';
+import Exchange from 'src/currency.js';
 
-function convertMoney(dollars, rate) {
-  return dollars * rate;
-}
+$(document).ready(function(event) {
+  event.preventDefault();
+  $("#exchange").submit(function(event) {
+    event.preventDefault();
+
+    const dollars = $("input#usDollars").val();
+    const convertTo = $("#currencies").val();
+
+    $("#welcome").hide();
+    $("#welcomeTwo").hide();
+
+    Exchange.getRates(convertTo, dollars)
+      .then(function(response) {
+        if (response instanceof Error) {
+          throw response;
+        }
+        if (response["error-type"] === "unsupported-code") {
+          showErrorTwo();
+        } else {
+          const total = response.conversion_result;
+          showResult(dollars, total);
+        }
+      })
+      .catch(function(error) {
+        showError(error.message);
+      });
+  });
+});
+
+  function showError(error) {
+    $("#error").show();
+    $("#errorMessage").text(error);
+  }
+
+  function showErrorTwo() {
+    $("#errorTwo").show();
+  }
+
+  function showResult(dollars, total) {
+    $("#results").show();
+    $(".usDollars").text(dollars);
+    $(".convertedMoney").text(total);
+  }
