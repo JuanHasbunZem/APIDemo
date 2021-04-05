f366ca9f4f1b5a10b3dccd9caa7fad1b5c51de86
JuanHasbunZem <zemenarwow@gmail.com>
2021-04-04 22:31:24 -0700

    making a commit here before changing code to account for new Pair fetch, in case I want to go back

--- src/main.js
+++ src/main.js
@@ -3,3 +3,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Exchange from './currency';
+
+function convertMoney(dollars, rate) {
+  return dollars * rate;
+}
